205836d249793197d85606f552cfabf8b1cae5fe
gustavjung01 <gustavjung01@gmail.com>
2026-07-03 13:03:45 +0700

    feat: add reusable filter bar component

---
+++ src/ui/layout/FilterBar.tsx
@@ -0,0 +1,24 @@
+type FilterItem = {
+  label: string;
+  value: string;
+};
+
+type FilterBarProps = {
+  title?: string;
+  filters: FilterItem[];
+};
+
+export function FilterBar({ title = "Bo loc nhanh", filters }: FilterBarProps) {
+  return (
+    <div className="filter-bar">
+      <strong>{title}</strong>
+      <div className="filter-list">
+        {filters.map((filter) => (
+          <span className="filter-chip" key={`${filter.label}-${filter.value}`}>
+            {filter.label}: <b>{filter.value}</b>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
